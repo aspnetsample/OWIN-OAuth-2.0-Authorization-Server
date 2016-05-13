fd01a43c9270a46ba97fbf3a2823a0fdbc4bc8f8
刘宇 <helloworldliu@hotmail.com>
2016-05-13 17:50:48 +0800

    no message

---
+++ C#/ResourceServer/Controllers/MeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ResourceServer.Controllers
+{
+    //[EnableCors(origins: "http://mywebclient.azurewebsites.net", headers: "*", methods: "*")]
+    [Authorize]
+    public class MeController : ApiController
+    {
+        public string Get()
+        {
+            return this.User.Identity.Name;
+        }
+    }
+}
